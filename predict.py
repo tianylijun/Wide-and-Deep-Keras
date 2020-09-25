074550b9a6f918da79403b662e077db20b33e804
jun.li <jun.li@streamcomputing.com>
2020-09-25 20:46:18 +0800

    Signed-off-by: jun.li <jun.li@streamcomputing.com>

--- predict.py
+++ predict.py
@@ -1,81 +1,134 @@
-import csv
+import os
 
 import numpy as np
-import pandas as pd
-import os
 import argparse
-import datetime
 import tensorflow as tf
 from keras import backend as K
-from tensorflow.python.keras.layers import Embedding
-from tensorflow.python.keras.regularizers import l2
 
 
-def cross_columns(x_cols):
-    """simple helper to build the crossed columns in a pandas dataframe
+# Create function to convert saved keras model to tensorflow graph
+def convert_to_pb(weight_file, input_fld='', output_fld=''):
+    import os
+    import os.path as osp
+    from tensorflow.python.framework import graph_util
+    from tensorflow.python.framework import graph_io
+    from keras.models import load_model
+    from keras import backend as K
+
+    # weight_file is a .h5 keras model file
+    output_node_names_of_input_network = ["pred0"]
+    output_node_names_of_final_network = 'output_node'
+
+    # change filename to a .pb tensorflow file
+    output_graph_name = weight_file[:-2] + 'pb'
+    weight_file_path = osp.join(input_fld, weight_file)
+
+    net_model = load_model(weight_file_path)
+
+    num_output = len(output_node_names_of_input_network)
+    pred = [None] * num_output
+    pred_node_names = [None] * num_output
+
+    for i in range(num_output):
+        pred_node_names[i] = output_node_names_of_final_network + str(i)
+        pred[i] = tf.identity(net_model.output[i], name=pred_node_names[i])
+
+    sess = K.get_session()
+
+    constant_graph = graph_util.convert_variables_to_constants(sess, sess.graph.as_graph_def(), pred_node_names)
+    graph_io.write_graph(constant_graph, output_fld, output_graph_name, as_text=False)
+    print('saved the constant graph (ready for inference) at: ', osp.join(output_fld, output_graph_name))
+
+    return output_fld + output_graph_name
+
+
+def freeze_session(session, keep_var_names=None, output_names=None, clear_devices=True):
     """
-    crossed_columns = dict()
-    colnames = ['_'.join(x_c) for x_c in x_cols]
-    for cname, x_c in zip(colnames, x_cols):
-        crossed_columns[cname] = x_c
-    return crossed_columns
+    Freezes the state of a session into a pruned computation graph.
+
+    Creates a new computation graph where variable nodes are replaced by
+    constants taking their current value in the session. The new graph will be
+    pruned so subgraphs that are not necessary to compute the requested
+    outputs are removed.
+    @param session The TensorFlow session to be frozen.
+    @param keep_var_names A list of variable names that should not be frozen,
+                          or None to freeze all the variables in the graph.
+    @param output_names Names of the relevant graph outputs.
+    @param clear_devices Remove the device directives from the graph for better portability.
+    @return The frozen graph definition.
+    """
+    from tensorflow.python.framework.graph_util import convert_variables_to_constants
+    graph = session.graph
+    with graph.as_default():
+        freeze_var_names = list(set(v.op.name for v in tf.global_variables()).difference(keep_var_names or []))
+        output_names = output_names or []
+        output_names += [v.op.name for v in tf.global_variables()]
+        # Graph -> GraphDef ProtoBuf
+        input_graph_def = graph.as_graph_def()
+        if clear_devices:
+            for node in input_graph_def.node:
+                node.device = ""
+        frozen_graph = convert_variables_to_constants(session, input_graph_def,
+                                                      output_names, freeze_var_names)
+        return frozen_graph
 
 
-def wide(df_train, df_test, wide_cols, x_cols, target):
-    df_wide = df_test
+def load_graph(graph_def):
+    wide = tf.placeholder(shape=(1, 762), dtype='float', name='wide')
+    workclass_inp = tf.placeholder(shape=(1, 1), dtype='int32', name='workclass_inp')
+    education_inp = tf.placeholder(shape=(1, 1), dtype='int32', name='education_inp')
+    marital_status_inp = tf.placeholder(shape=(1, 1), dtype='int32', name='marital_status_inp')
+    occupation_inp = tf.placeholder(shape=(1, 1), dtype='int32', name='occupation_inp')
+    relationship_inp = tf.placeholder(shape=(1, 1), dtype='int32', name='relationship_inp')
+    race_inp = tf.placeholder(shape=(1, 1), dtype='int32', name='race_inp')
+    gender_inp = tf.placeholder(shape=(1, 1), dtype='int32', name='gender_inp')
+    native_country_inp = tf.placeholder(shape=(1, 1), dtype='int32', name='native_country_inp')
+    age_in = tf.placeholder(shape=(1, 1), dtype='float', name='age_in')
+    capital_gain_in = tf.placeholder(shape=(1, 1), dtype='float', name='capital_gain_in')
+    capital_loss_in = tf.placeholder(shape=(1, 1), dtype='float', name='capital_loss_in')
+    hours_per_week_in = tf.placeholder(shape=(1, 1), dtype='float', name='hours_per_week_in')
+    tf.import_graph_def(graph_def, name='',
+                        input_map={"wide": wide,
+                                   "workclass_inp": workclass_inp, "education_inp": education_inp,
+                                   "marital_status_inp": marital_status_inp, "occupation_inp": occupation_inp,
+                                   "relationship_inp": relationship_inp, "race_inp": race_inp, "gender_inp": gender_inp,
+                                   "native_country_inp": native_country_inp,
+                                   "age_in": age_in, "capital_gain_in": capital_gain_in,
+                                   "capital_loss_in": capital_loss_in,
+                                   "hours_per_week_in": hours_per_week_in})
+    return
 
-    crossed_columns_d = cross_columns(x_cols)
-    categorical_columns = list(
-        df_wide.select_dtypes(include=['object']).columns)
 
-    wide_cols += list(crossed_columns_d.keys())
+def load_frozen_model_v1(pb_path, prefix='', print_nodes=False):
+    """Load frozen model (.pb file) for testing.
+    After restoring the model, operators can be accessed by
+    graph.get_tensor_by_name('<prefix>/<op_name>')
+    Args:
+        pb_path: the path of frozen model.
+        prefix: prefix added to the operator name.
+        print_nodes: whether to print node names.
+    Returns:
+        graph: tensorflow graph definition.
+    """
+    if os.path.exists(pb_path):
+        with tf.io.gfile.GFile(pb_path, "rb") as f:
+            graph_def = tf.GraphDef()
+            graph_def.ParseFromString(f.read())
+        with tf.Graph().as_default() as graph:
+            tf.import_graph_def(
+                graph_def,
+                name=prefix
+            )
+            if print_nodes:
+                for op in graph.get_operations():
+                    print(op.name)
+            return graph
+    else:
+        print('Model file does not exist', pb_path)
+        exit(-1)
 
-    for k, v in crossed_columns_d.items():
-        df_wide[k] = df_wide[v].apply(lambda x: '-'.join(x), axis=1)
 
-    df_wide = df_wide[wide_cols]
-
-    dummy_cols = [
-        c for c in wide_cols if c in categorical_columns + list(crossed_columns_d.keys())]
-    df_wide = pd.get_dummies(df_wide, columns=[x for x in dummy_cols])
-
-    test = df_wide
-
-    cols = [c for c in test.columns if c != target]
-    X_test = test[cols].values
-    y_test = test[target].values.reshape(-1, 1)
-    return X_test, y_test
-
-
-if __name__ == '__main__':
-    print(tf.__version__)
-    ap = argparse.ArgumentParser()
-    ap.add_argument("--pb_file", type=str, default="model_1_13/saved_model.h5")
-    args = vars(ap.parse_args())
-    pb_file = args["pb_file"]
-
-    # inp = 1
-    # em = Embedding(9, 8, input_length=1, embeddings_regularizer=l2(1e-3))
-    # l = len(em.get_weights())
-    # out = em(0)
-    # out = em(1)
-    # out = em(2)
-    # out = em(3)
-    # out = em(4)
-    # out = em(5)
-    # out = em(6)
-    # out = em(7)
-    # out = em(8)
-    # K.set_learning_phase(0)
-    # print(tf.keras.backend.learning_phase())
-    # model = tf.keras.models.load_model(pb_file)
-    model = tf.keras.models.load_model(pb_file, compile=False)
-    # model.summary()
-    inp = model.input
-    print(inp)
-    output = model.output
-    print(output)
-
+def load_input():
     wide = np.loadtxt("data/wide.txt")
     wide = wide.astype(dtype=np.int32).reshape([1, len(wide)])
     deep = np.loadtxt("data/deep.txt")
@@ -91,9 +144,57 @@ if __name__ == '__main__':
     capital_gain_in = deep[9].astype(dtype=np.float).reshape([1, 1])
     capital_loss_in = deep[10].astype(dtype=np.float).reshape([1, 1])
     hours_per_week_in = deep[11].astype(dtype=np.float).reshape([1, 1])
+    return wide, workclass_inp, education_inp, marital_status_inp, occupation_inp, relationship_inp, race_inp, gender_inp, native_country_inp, age_in, capital_gain_in, capital_loss_in, hours_per_week_in
 
-    inputs = [wide, workclass_inp, education_inp, marital_status_inp, occupation_inp, relationship_inp, race_inp,
-              gender_inp, native_country_inp, age_in, capital_gain_in, capital_loss_in, hours_per_week_in]
 
-    output = model.predict(inputs)  # n : 0.02082383
+if __name__ == '__main__':
+    ap = argparse.ArgumentParser()
+    ap.add_argument("--pb_file", type=str, default="model_1_13/saved_model.h5")
+    args = vars(ap.parse_args())
+    pb_file = args["pb_file"]
+
+    model = tf.keras.models.load_model(pb_file, compile=False)
+    # model.summary()
+    inp = model.input
+    print(inp)
+    output = model.output
     print(output)
+
+    output = model.predict(load_input())  # p: 0.50864685
+    print(output)
+    print(tf.__version__)
+
+    sess = K.get_session()
+    kgraph = sess.graph
+    # for op in kgraph.get_operations():
+    #     print(op.name)
+    graph_def = kgraph.as_graph_def()
+    load_graph(graph_def)
+    wide_data, workclass_inp_data, education_inp_data, marital_status_inp_data, occupation_inp_data, relationship_inp_data, \
+    race_inp_data, gender_inp_data, native_country_inp_data, age_in_data, capital_gain_in_data, \
+    capital_loss_in_data, hours_per_week_in_data = load_input()
+
+    predictions = sess.run(sess.graph.get_tensor_by_name('wide_deep/Sigmoid:0'), {
+        'hours_per_week_in:0': hours_per_week_in_data,
+        'capital_loss_in:0': capital_loss_in_data,
+        'capital_gain_in:0': capital_gain_in_data,
+        'age_in:0': age_in_data,
+        'native_country_inp:0': native_country_inp_data,
+        'gender_inp:0': gender_inp_data,
+        'race_inp:0': race_inp_data,
+        'relationship_inp:0': relationship_inp_data,
+        'occupation_inp:0': occupation_inp_data,
+        'marital_status_inp:0': marital_status_inp_data,
+        'education_inp:0': education_inp_data,
+        'workclass_inp:0': workclass_inp_data,
+        'wide:0': wide_data})
+    print(predictions)
+
+    # output = tf.placeholder(shape=(1, 1), dtype='float', name='wide_deep/Sigmoid')
+    # tf.keras.backend.set_learning_phase(0)
+    # tf.saved_model.simple_save(sess, 'model2',
+    #                            inputs={inp[0].name: inp[0], inp[1].name: inp[1], inp[2].name: inp[2],
+    #                                    inp[3].name: inp[3], inp[4].name: inp[4], inp[5].name: inp[5],
+    #                                    inp[6].name: inp[6], inp[7].name: inp[7], inp[8].name: inp[8],
+    #                                    inp[9].name: inp[9], inp[10].name: inp[10], inp[11].name: inp[11]},
+    #                            outputs={"wide_deep/Sigmoid": output})
